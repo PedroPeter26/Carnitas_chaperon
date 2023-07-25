2575f4a857dde5b3f6c9e8761099bb2f3a99876c
Valeria Morales <134093672+valeriam16@users.noreply.github.com>
2023-07-25 12:41:07 -0600

    Add files via upload

---
+++ class/bd.php
@@ -0,0 +1,9 @@
+<?php 
+class Database
+{
+    private $PDOLocal;
+    private $user = "root";/*El usuario q esta en mi servidor de bd*/
+    private $password = "";
+    private $server = "mysql:host=localhost; dbname=carnitaschaperon";
+}
+?>
