15010e53e0deee2e82ec69703cb9bc26cc5f28ed
JonathanARR <134093028+JonathanARR@users.noreply.github.com>
2023-08-15 08:35:40 -0600

    Más cambios simpáticos

--- views/ordenes/ordenes_online.php
+++ views/ordenes/ordenes_online.php
@@ -5,90 +5,159 @@ $db = new database();
 $db->conectarDB();
 $pdo = $db->getConexion();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Belanosima&family=Lilita+One&display=swap" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Órdenes Online</title>
     <?php include '../headadmin.php'; ?>
-    <title>Reportes diarios</title>
+    <!-- Incluye los archivos de estilo y scripts necesarios aquí -->
 </head>
 
 <body>
-    <!--BARRA DE NAV-->
     <?php include '../sidebaradmin.php'; ?>
 
-    <!--CONTENIDO-->
-    <div class="content-wrapper" style="background-color: white;">
-        <div class="container p-4">
-            <h1>Órdenes Online</h1>
+    <div class="content-wrapper">
+        <br>
+        <div class="row">
             <?php
             try {
-                $sql = "SELECT noti, user_id, USUARIOS.nombre as 'n', orden_id as 'o', notification_dataadmin.description as dd, PRODUCTOS.nombre as p, SUM(DETALLE_ORDEN.cantidad) as c, PRODUCTOS.precio_app as precio, SUM(PRODUCTOS.precio_app * DETALLE_ORDEN.cantidad) as subtotal
-                FROM USUARIOS 
-                JOIN ORDENES ON USUARIOS.user_id = ORDENES.cliente 
-                JOIN notification_data ON notification_data.orden = ORDENES.orden_id 
-                JOIN notification_dataadmin ON notification_dataadmin.noti = notification_data.id 
-                JOIN DETALLE_ORDEN ON DETALLE_ORDEN.orden = ORDENES.orden_id 
-                JOIN PRODUCTOS ON PRODUCTOS.producto_id = DETALLE_ORDEN.producto 
-                WHERE ORDENES.status = 'Proceso' GROUP BY PRODUCTOS.nombre 
-                ORDER BY o DESC";
-
-                $sql = $pdo->prepare($sql);
+                $sql = $pdo->prepare("SELECT
+                ORDENES.orden_id,
+                USUARIOS.user AS Cliente,
+                PRODUCTOS.nombre AS Nombre_Producto,
+                ORDENES.hora_inicio as 'hi',
+                ORDENES.fecha,
+                PRODUCTOS.precio_app,
+                SUM(DETALLE_ORDEN.cantidad) AS Cantidad_Total,
+                (PRODUCTOS.precio_app * SUM(DETALLE_ORDEN.cantidad)) AS Subtotal
+                FROM ORDENES
+                INNER JOIN USUARIOS ON ORDENES.cliente = USUARIOS.user_id
+                INNER JOIN DETALLE_ORDEN ON ORDENES.orden_id = DETALLE_ORDEN.orden
+                INNER JOIN PRODUCTOS ON DETALLE_ORDEN.producto = PRODUCTOS.producto_id
+                WHERE ORDENES.status = 'Proceso'
+                GROUP BY ORDENES.orden_id, PRODUCTOS.nombre
+                ORDER BY ORDENES.orden_id DESC;");
                 $sql->execute();
-                $orders = $sql->fetchAll(PDO::FETCH_ASSOC);
+                $result = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-                $currentOrder = null;
-                $total = 0;
+                foreach ($result as $row) {
+                    $orden_id = $row["orden_id"];
+                    $fecha_orden = $row["fecha"];
+                    $nombre_usuario = $row["Cliente"];
+                    $hora_inicio = $row["hi"];
+                    $total = 0;
+            ?>
 
-                if ($orders) {
-                    foreach ($orders as $order) {
-                        if ($currentOrder !== $order['o']) {
-                            if ($currentOrder !== null) {
-                                echo '<p>Total de la Orden: $' . number_format($total,2) . '</p>'; // Muestro el total de la orden
-                                echo '<hr class="dropdown-divider"></hr>';
-                                echo "</div>";
-                                $total = 0; // Se reinicia el total para la que sigue
-                            }
 
-                            $currentOrder = $order['o'];
 
-                            echo "<div class='border p-4' style='background-color: red;'";
-                            echo "<h2>Orden " . $order['o'] . "</h2>";
-                            echo "<h3>Cliente: " . $order['n'] . "</h3>";
-                            echo '<hr class="dropdown-divider"></hr>';
-                        }
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <div class="card ms-4 me-4 p-3">
+                            <div class="card-header">
+                                <h4>Orden ID: <?php echo $orden_id; ?></h4>
+                                <p>Fecha de Orden: <?php echo $fecha_orden; ?> Hora: <?php echo $hora_inicio ?></p>
+                            </div>
+                            <div class="card-body">
+                                <h5>Cliente: <b> <?php echo $nombre_usuario; ?> </b> </h5>
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle<?php echo $orden_id; ?>" data-bs-toggle="modal">Más información</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                        echo '<a class="dropdown-item">' . $order['p'] . ' ---------------- ' . $order['c'] . ' ---------------- ' . $order['subtotal'] . '</a>';
-                        $total += $order['subtotal'];
-                    }
+                    <!-- Modal for the order <?php echo $orden_id; ?> -->
+                    <div class="modal fade" id="exampleModalToggle<?php echo $orden_id; ?>" aria-hidden="true" role="dialog" aria-labelledby="myModalLabel" tabindex="-1">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Detalle de la orden</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <table class='table table-striped table-warning table-hover'>
+                                        <thead>
+                                            <tr>
+                                                <th scope='col'>Producto</th>
+                                                <th scope='col'>Precio</th>
+                                                <th scope='col'>Cantidad</th>
+                                                <th scope='col'>Subtotal</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            //Muestra una orden por producto de orden
+                                            foreach ($result as $row1) {
+                                                if ($row1["orden_id"] == $orden_id) {
+                                                    $producto = $row1["Nombre_Producto"];
+                                                    $precio = $row1["precio_app"];
+                                                    $cantidad = $row1["Cantidad_Total"];
+                                                    $subtotal = $row1["Subtotal"];
+                                                    echo "<tr>
+                                                        <td>$producto</td>
+                                                        <td>$precio</td>
+                                                        <td>$cantidad</td>
+                                                        <td>$subtotal</td>
+                                                    </tr>";
+                                                }
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-success" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Finalizar pedido</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                    echo '<p>Total de la Orden: ' . $total . '</p>'; // Muestro el total de la última orden
-                    echo '<hr class="dropdown-divider"></hr><br>';
-                    echo "</div>";
-                
-                } else {
-                    echo '<a class="dropdown-item">No hay órdenes pendientes.</a>';
+                    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Advertencia</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Si está seguro de que el pedido fue completado correctamente dele click a "Continuar", en caso contrario puede pulsar a "Regresar".
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-warning me-auto" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Regresar</button>
+                                    <button class="btn btn-success ms-auto" type="submit" name="ejecutar" value="Ejecutar Procedimiento" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Continuar</button>
+                                    <?php
+                                    try {
+                                        //Falta corrección
+                                        $stmt = $pdo->prepare("CALL actualizarstatus_orden('Online', $orden_id, null)");
+                                        $stmt->execute();
+
+                                        echo "Se actualizó con éxito.";
+                                    } catch (PDOException $e) {
+                                        echo "Error: " . $e->getMessage();
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+            <?php
                 }
             } catch (PDOException $e) {
-                echo '<a class="dropdown-item text-danger">Error: ' . $e->getMessage() . '</a>';
+                echo "Error: " . $e->getMessage();
             }
             ?>
 
         </div>
+        <br>
     </div>
 
     <?php include '../footeradmin.php'; ?>
-    <?php $db->desconectarDB(); ?>
+    <!-- Incluye los scripts necesarios aquí -->
 </body>
 
 </html>
